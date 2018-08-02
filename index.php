11197062a8e3a393719f48d334d60a78246226c6
José Carlos de Camargo <jccamargo15@gmail.com>
2018-08-01 22:18:11 -0300

    inserção e exclusão

--- index.php
+++ index.php
@@ -1,19 +1,52 @@
 <?php  
 	$link = mysql_connect('localhost', 'root', '');
 
-	$link = mysql_select_db('web2', $link);
+	mysql_select_db('web2', $link);
+
+	$valorNome = $valorEmail = $valorPhone = $valorPhoto = $valorSenha = '';
 
 
 	if (isset($_GET['action']) and !empty($_GET['action'])) {
 		$action = $_GET['action'];
-		$id = $_GET['id'];
 
 		if ($action == 'delet') {
-			mysql_query("DELETE FROM tb_users WHERE id_user = " . $id);
+			$id = $_GET['id'];
+			mysql_query("DELETE FROM tb_users WHERE id_user = " . $id, $link);
+		}
+
+		if ($action == 'edit') {
+			$id = $_GET['id'];
+			$query = "SELECT * FROM tb_users WHERE id_user = " . $id;
+			$edita = mysql_fetch_assoc(mysql_query($query, $link));
+			$valorNome = $edita['name'];
+			$valorEmail = $edita['email'];
+			$valorPhone = $edita['phone'];
+			$valorPhoto = $edita['photo'];
+			$valorSenha = $edita['pass'];
+		}
+
+		if ($action == 'insert') {
+			$nome = $_POST['nome'];
+			$email = $_POST['email'];
+			$telefone = $_POST['phone'];
+			$pass = md5($_POST['senha']);
+			$name = $_FILES['photo']['name'];
+			$tmp_name = $_FILES['photo']['tmp_name'];
+			$nomefoto = date("YmdHis").'_'.$name;
+
+			// $dir = '../img/';
+			// move_uploaded_file($tmp_name, $dir.$nomefoto);
+
+			$query = 'INSERT INTO tb_users (name, email, phone, photo, pass) VALUES ("'.$nome.'", "'.$email.'",  "'.$telefone.'", "'.$nomefoto.'", "'.$pass.'")';
+
+			mysql_query($query, $link) or die(mysql_error());
 		}
 	}
+	else {
+		$action = 'insert';
+	}
 
-	$result = mysql_query("SELECT * FROM tb_users");
+	$result = mysql_query("SELECT * FROM tb_users", $link);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,6 +58,17 @@
 
 	<h1>Revisão Programação Web 01</h1>
 
+	<form action="index.php?action=<?php echo $action; ?>" method="post">
+		Nome: <input type="text" name="nome" value="<?php echo $valorNome; ?>"> 
+		E-mail: <input type="email" name="email" value="<?php echo $valorEmail; ?>"> 
+		Telefone: <input type="text" name="phone" value="<?php echo $valorPhone; ?>"> 
+		Senha: <input type="pass" name="senha" value="<?php echo $valorSenha; ?>">
+		Foto: <input type="file" name="photo">
+		<input type="submit" value="Enviar">
+	</form>
+
+	<hr>
+
 	<table border="1">
 		<tr>
 			<td>Código</td>
@@ -34,6 +78,7 @@
 			<td>Foto</td>
 			<td>Senha</td>
 			<td>Deletar</td>
+			<td>Editar</td>
 		</tr>
 		<?php
 			while ($linha = mysql_fetch_assoc($result)) {
@@ -45,6 +90,7 @@
 				echo '<td>' . $linha['photo'] . '</td>';
 				echo '<td>' . $linha['pass'] . '</td>';
 				echo '<td><a href="index.php?action=delet&id=' . $linha['id_user'] . '">Deletar</a></td>'; 
+				echo '<td><a href="index.php?action=edit&id=' . $linha['id_user'] . '">Editar</a></td>'; 
 				echo '</tr>';
 			}
 		?>
